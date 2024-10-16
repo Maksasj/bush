535a10654748c4810ee8ec4f03b1e27ca241253f
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-10-16 15:36:44 +0300

    piping

--- main.c
+++ main.c
@@ -4,14 +4,28 @@
 #include <sys/wait.h>
 
 int main() {
+    int p[2];
+    if(pipe(p) < 0)
+        fprintf(stderr, "Failed to create pipe");
+
     pid_t id = fork();
 
     if(id == 0) {
-        char* args[] = { "echo", "Hello world", NULL };
+        close(STDOUT_FILENO);
+        dup(p[1]); 
+
         // Child
+        char* args[] = { "echo", "Hello wllld\n", NULL };
+
         execvp("echo", args); 
     } else {
         // Parent
+        printf("This is a parent process\n");
+
+        char buffer[200];
+        read(p[0], buffer, 18);
+
+        printf("This is from child process: '%s'\n", buffer);
     }
 
     int status;
@@ -19,6 +33,9 @@ int main() {
 
     if(!WIFEXITED(status))
         fprintf(stderr, "Child process terminated abnormally\n");
+
+    close(p[0]);
+    close(p[1]);
 
     return 0;
 }
